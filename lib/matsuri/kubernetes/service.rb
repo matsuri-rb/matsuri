98d2cb2723b8ed6958f51e5fdab0971056aa99cf
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-09-22 01:22:20 -0700

    Expanded basic Services dsl to include service types, and nodePort

--- lib/matsuri/kubernetes/service.rb
+++ lib/matsuri/kubernetes/service.rb
@@ -7,21 +7,24 @@ module Matsuri
       let(:spec) do
         {
           ports: ports,
-          selector: selector
+          selector: selector,
+          type: service_type
         }
       end
 
-      let(:ports)    { [port(port_num)] }
-      let(:port_num) { fail NotImplementedError, 'Must define let(:port_num)' }
-      let(:selector) { fail NotImplementedError, 'Must define let(:selector)' }
+      let(:ports)        { [port(port_num)] }
+      let(:port_num)     { fail NotImplementedError, 'Must define let(:port_num)' }
+      let(:selector)     { fail NotImplementedError, 'Must define let(:selector)' }
+      let(:service_type) { 'ClusterIP' }
 
       # Helpers
       # Optional:
       #  target_port: the port exposed externally
       #  protocol:    connection protocol (ex: tcp)
-      def port(num, target_port: nil, protocol: nil, name: nil)
+      def port(num, target_port: nil, protocol: nil, name: nil, node_port: nil)
         _port = { port: num }
         _port[:targetPort] = target_port   if target_port
+        _port[:nodePort]   = node_port     if node_port
         _port[:protocol]   = protocol.to_s if protocol
         _port[:name]       = name.to_s     if name
         return _port
