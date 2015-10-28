038451623f1af560ed05c485f7ddc823fec30978
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-10-28 02:54:04 -0700

    Added Pod#up? and get running status from kubernetes

--- lib/matsuri/kubernetes/pod.rb
+++ lib/matsuri/kubernetes/pod.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module Matsuri
   module Kubernetes
     class Pod < Matsuri::Kubernetes::Base
@@ -31,6 +33,16 @@ module Matsuri
       let(:mem_request) { cpu_limit }
 
       # Helper methods
+      def up?
+        cmd = kubectl "--namespace=#{namespace} get #{resource_type}/#{name} -o json", no_stdout: true
+        Matsuri.log :fatal, "Unable to get status for #{resource_type}/#{name}" unless cmd.status.success?
+        pod_json = JSON.parse(cmd.stdout)
+        Matsuri.log :debug, pod_json['status']
+        return false unless pod_json['status']['phase'] == 'Running'
+        return false unless pod_json['status']['conditions'].any?
+        pod_json['status']['conditions'].all? { |c| c['type'] == 'Ready' && c['status'] == 'True' }
+      end
+
       def config_file(path)
         File.join config.config_path, path
       end
