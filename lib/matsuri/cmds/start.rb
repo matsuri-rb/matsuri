472a58e2db5172780779866a5b3026f7993f6717
Ho-Sheng Hsiao <hosh@legal.io>
2016-11-02 23:10:59 +0000

    [CMDS] Cleaned up start cmd

--- lib/matsuri/cmds/start.rb
+++ lib/matsuri/cmds/start.rb
@@ -5,6 +5,12 @@ module Matsuri
 
       class_option :'no-deps', type: :boolean, default: false
 
+      def self.start_cmd_for(resource_name)
+        define_method(resource_name) do |name|
+          start_resource { Matsuri::Registry.send(resource_name, name).new }
+        end
+      end
+
       desc 'dns', 'starts cluster dns'
       def dns
         with_config do |_|
@@ -27,29 +33,13 @@ module Matsuri
       end
 
       desc 'service SERVICE_NAME', 'start a service'
-      def service(name)
-        with_config do |opt|
-          Matsuri::Registry.service(name).new.start!
-        end
-      end
+      start_cmd_for :service
 
       desc 'endpoints ENDPOINTS_NAME', 'start an endpoint set'
-      def endpoints(name)
-        with_config do |opt|
-          Matsuri::Registry.endpoints(name).new.start!
-        end
-      end
+      start_cmd_for :endpoints
 
       desc 'secret SECRET_NAME', 'upload a secret'
-      def secret(name)
-        start_resource { Matsuri::Registry.secret(name).new.start! }
-      end
-
-      def self.start_cmd_for(resource_name)
-        define_method(resource_name) do |name|
-          start_resource { Matsuri::Registry.send(resource_name, name).new }
-        end
-      end
+      start_cmd_for :secret
 
       desc 'pv PV_NAME', 'start a persistent volume'
       start_cmd_for :pv
