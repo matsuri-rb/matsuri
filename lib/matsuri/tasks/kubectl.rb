94116fb22eff016125b357f32820809f3500e648
Ho-Sheng Hsiao <hosh@legendsoflearning.com>
2019-12-09 18:02:06 -0700

    [TASKS] Support AWS style kubeconfig generated from eksctl

--- lib/matsuri/tasks/kubectl.rb
+++ lib/matsuri/tasks/kubectl.rb
@@ -6,12 +6,13 @@ module Matsuri
       let(:environment)  { Matsuri.environment }
       let(:env_config)   { Matsuri::Platform.send(environment) }
       let(:namespace)    { env_config.namespace || 'default' }
-      let(:user)         { env_config.user || kube_context }
+      let(:kube_user)    { env_config.user || kube_context }
+      let(:kube_cluster) { env_config.kube_cluster || kube_context }
 
       # Set up aliases for kubectl. This allows you you to use
       # `kubectl --context=staging` instead of the name given by GKE
       def setup!
-        shell_out! "kubectl config set-context #{environment} --cluster=#{kube_context} --namespace=#{namespace} --user=#{user}"
+        shell_out! "kubectl config set-context #{environment} --cluster=#{kube_cluster} --namespace=#{namespace} --user=#{kube_user}"
       end
     end
   end
